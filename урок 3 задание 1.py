d1dd2933e76c0c99351cb0dd8d91eb27b79b4212
RyabsevEgor <133496781+RyabsevEgor@users.noreply.github.com>
2023-05-20 14:36:55 +0500

    Add files via upload

---
+++ урок 3 задание 1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 20 12:08:45 2023
+
+@author: Егор
+"""
+vid = input()
+name = input()
+age = input()
+print('Это', vid, 'по кличке', '"' + name + '".','Возраст:', age + '.')
