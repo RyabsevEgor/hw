288e13cca1a063130e820f385cc083c860805514
RyabsevEgor <133496781+RyabsevEgor@users.noreply.github.com>
2023-05-20 14:36:24 +0500

    Add files via upload

---
+++ урок 3 задание 2.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 20 11:45:42 2023
+
+@author: Егор
+"""
+a1 = input()
+a2 = input()
+a3 = input()
+print(a1, a2, a3, sep='=>')
---
+++ урок 4 задание 1.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 20 12:10:58 2023
+
+@author: Егор
+"""
+a = float(input())
+b = float(input())
+print('Площадь =', a*b, 'Периметр =', 2*(a+b))
