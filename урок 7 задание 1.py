4e97ec076c8f79f3d3bf35738051a75c5a3b9b35
RyabsevEgor <133496781+RyabsevEgor@users.noreply.github.com>
2023-05-21 16:52:17 +0500

    Add files via upload

---
+++ урок 7 задание 1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 21 16:48:16 2023
+
+@author: Егор
+"""
+s = input()
+if s == s[::-1]:
+  print('yes')
+else:
+  print('no')
---
+++ урок 7 задание 2.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 21 16:48:53 2023
+
+@author: Егор
+"""
+s = input().split()
+a = ''
+for i in range(len(s)):
+  a += s[i]+ ' '
+print(a)
