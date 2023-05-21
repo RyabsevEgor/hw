74d49e788754018c3c289ccbeae99446ec32144c
RyabsevEgor <133496781+RyabsevEgor@users.noreply.github.com>
2023-05-21 20:06:55 +0500

    Add files via upload

--- урок 8 задание 2.py
+++ урок 8 задание 2.py
@@ -1,16 +1,8 @@
-m = int(input())
-n = int(input())
-A = []
-for i in range(n):
-  A.append(int(input()))
-#сортировка по возрастанию
-A.sort()
-c = 0
-while len(A)>1:
-  sum = 0
-  while sum + A[0] < m and len(A)>1:
-    sum += A[0]
-    del(A[0])
-  c+=1
-
-print('минимальное количество лодок:',c)
+N = int(input())
+a = []
+b = []
+for i in range(N):
+  a.append(int(input()))
+b.append(a[N-1])
+b.extend(a[:N-1])
+print(b)
