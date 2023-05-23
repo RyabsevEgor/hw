4143860a50727079709e04f40c64d1f7a73db477
RyabsevEgor <133496781+RyabsevEgor@users.noreply.github.com>
2023-05-23 22:50:58 +0500

    Add files via upload

--- урок 10 задание 1.py
+++ урок 10 задание 1.py
@@ -12,16 +12,16 @@ data = {'Вид питомца': vid, 'Возраст питомца': age,
           'Имя владельца': owner_name}
 pets[pet_name] = data
 
-if int(data['Возраст питомца'])%10 == 1:
+if int(pets[pet_name]['Возраст питомца'])%10 == 1:
   age_ending = 'год'
-elif 1 < int(data['Возраст питомца'])%10 < 5:
+elif 1 < int(pets[pet_name]['Возраст питомца'])%10 < 5:
   age_ending = 'года'
-elif 4 < int(data['Возраст питомца'])%10 < 10:
+elif 4 < int(pets[pet_name]['Возраст питомца'])%10 < 10:
   age_ending = 'лет'
 
 for key in pets.keys():
    name = (key)
 
 print('Это', pets[name]['Вид питомца'], 'по кличке', '"' + name
-+ '".','Возраст питомца:', str(pets[name]['Возраст питомца']), age_ending
++ '".','Возраст питомца:', pets[name]['Возраст питомца'], age_ending
 + '.', 'Имя владельца:', pets[name]['Имя владельца'])
