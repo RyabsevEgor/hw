38eb3893ad881e90191ebb7f5c7e08ee3868ecda
RyabsevEgor <133496781+RyabsevEgor@users.noreply.github.com>
2023-07-08 22:35:24 +0500

    Add files via upload

---
+++ Алгоритмы/Алгоритмы 7 (односвязный список).py
@@ -0,0 +1,74 @@
+# односвязный список
+class Node:
+    def __init__(self):
+        self.value = None
+        self.next = None
+class LinkedList:
+    def __init__(self):
+        self.head = Node()
+
+    def add(self, value):
+        if self.head.value is None:
+            self.head.value = value
+            return
+
+        temp = Node()
+        temp.value = value
+        # 1 option
+        ''''
+        current_node = self.head
+        while current_node.next is not None:
+            current_node = current_node.next
+        current_node.next = temp 
+        '''
+        # 2 option
+        temp.next = self.head
+        self.head = temp
+    def lenght(self):
+        current_node = self.head
+        if self.head is None:
+            size = 0
+        else:
+            size = 1
+        while current_node.next is not None:
+            current_node = current_node.next
+            size += 1
+        return size
+    
+    def find(self, value):
+        if self.head.value is not None:
+            if self.head.value == value:
+                return True
+        current_node = self.head
+        while current_node.next is not None:
+            current_node = current_node.next
+            if current_node.value == value:
+                return True
+        return False
+
+    def print(self):
+        if self.head.value is None:
+            print('пустой список')
+            return
+        print(self.head.value, end = ' ')
+        current_node = self.head
+        while current_node.next is not None:
+            
+            current_node = current_node.next
+            print(current_node.value, end = ' ')
+        print()
+LL = LinkedList()
+LL.add(9)
+LL.add(8)
+LL.add(6)
+LL.add(7)
+LL.add(56)
+LL.add(4)
+LL.add(11)
+print(LL.lenght())
+
+print(LL.find(9))
+print(LL.find(24))
+
+LL.print()
+a = 10
--- Алгоритмы/Алгоритмы 7 (очередь).py
+++ Алгоритмы/Алгоритмы 7 (очередь).py
@@ -1,6 +1,6 @@
-# очередь
+# очередь 
 from time import sleep
-class Queue:
+class Queue1:
     def __init__(self):
         self.elems = list()
     
@@ -15,12 +15,34 @@ class Queue:
         size = len(self.elems)
         return size
     
-queue = Queue()
+#очередь (добавление с конца)
+from time import sleep
+class Queue2:
+    def __init__(self):
+        self.elems = list()
+    
+    def add(self, value):
+        self.elems.insert(0, value)
 
-for i in range(10):
+    def get(self):
+        val = self.elems.pop()
+        return val
+
+    def size(self):
+        size = len(self.elems)
+        return size
+    
+
+
+
+queue = Queue2()
+
+for i in range(9, -1, -1):
     queue.add(i)
-
+    print(i, end = ' ')
+print()
 for i in range(queue.size()):
     val = queue.get()
     print(val)
     sleep(val)
+
